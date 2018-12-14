00a6739a183e903a96b3b47f2a8c680c095d42eb
bighai <chaoisgoodman@163.com>
2018-12-14 14:26:34 +0800

    intentservice练习

--- app/src/main/java/event/superman/com/androidmorethread/IntentServiceActivity.java
+++ app/src/main/java/event/superman/com/androidmorethread/IntentServiceActivity.java
@@ -1,29 +1,51 @@
 package event.superman.com.androidmorethread;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Toast;
 
 public class IntentServiceActivity extends AppCompatActivity {
-
+    Handler mUiHandler=new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            Toast.makeText(IntentServiceActivity.this,(String)msg.obj,Toast.LENGTH_SHORT).show();
+        }
+    };
+    private String toasts[] = {
+            "toast测试1",
+            "toast测试2",
+            "toast测试3",
+            "toast测试4",
+            "toast测试5",
+            "toast测试6"
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intent_service);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        MyIntentService.setCallbackListener(new MyIntentService.CallbackListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void update(Message msg) {//必须通过Handler去更新，该方法为异步方法，不可更新UI
+                Message message =Message.obtain();
+                message.what = msg.what;
+                message.obj = msg.obj;
+
+                mUiHandler.sendMessageDelayed(message,1000);
             }
         });
+        for (int i=0;i<toasts.length;i++){
+            Intent intent = new Intent(this,MyIntentService.class);
+            intent.putExtra(MyIntentService.TOAST_ACTION,toasts[i]);
+            intent.putExtra(MyIntentService.INDEX,i);
+            startService(intent);
+        }
     }
 
 }
---
+++ app/src/main/java/event/superman/com/androidmorethread/MyIntentService.java
@@ -0,0 +1,59 @@
+package event.superman.com.androidmorethread;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.os.Message;
+import android.support.annotation.Nullable;
+import android.telecom.Call;
+
+/**
+ * 作者 Superman
+ * 日期 2018/12/13 15:36.
+ * 文件 AndroidMoreThread
+ * 描述 四大组件activity和service是不能通过new来进行创建的。
+ * 四大组件是系统管理的
+ 组件要“注册”给系统才会有作用
+ 你只有“注册”或“不注册”的权利。
+ */
+
+public class MyIntentService extends IntentService {
+    public static final String TOAST_ACTION = "toast_action";
+    public static final String INDEX = "index";
+    public static CallbackListener listener;
+    public MyIntentService() {
+        super("MyIntentService");
+    }
+
+    /**
+     * 实现异步任务的方法
+     *
+     * @param intent Activity传递过来的Intent,数据封装在intent中
+     */
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+        String toast = intent.getStringExtra(TOAST_ACTION);
+        int index = intent.getIntExtra(INDEX,0);
+        try {
+            Thread.sleep(1000);
+            Message message = Message.obtain();
+            message.obj = toast+"。。经过耗时";
+            message.what = index;
+            //更新ui
+            if(listener!=null){
+                listener.update(message);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+    public static void setCallbackListener(CallbackListener listener1){
+        listener = listener1;
+    }
+    interface CallbackListener{
+        void update(Message msg);
+//        default void toast(){
+//
+//        }
+    }
+}
